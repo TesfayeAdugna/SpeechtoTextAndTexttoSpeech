16656479234cbdd6b5202aff5c7afabc1027adf2
Tesfaye Hordofa <adugnatesfaye888@gmail.com>
2024-06-13 09:32:20 +0300

    fix issue

--- lib/custom/selectionbutton.dart
+++ lib/custom/selectionbutton.dart
@@ -4,7 +4,7 @@ final ButtonStyle selectionBtn = ElevatedButton.styleFrom(
   minimumSize: Size(327, 50),
   shadowColor: Colors.grey,
   elevation: 15,
-  primary: Colors.black,
+  // primary: Colors.black,
   shape: const RoundedRectangleBorder(
     borderRadius: BorderRadius.all(
       Radius.circular(50),
